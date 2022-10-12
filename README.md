39edc83392d3fdad348328246d6fafeb1d88c1bb
Scott Ball <scotttball@gmail.com>
2022-10-12 10:44:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Branch naming rules
+
+Github action to enforce naming convention on branch names
+
+## Usage
+
+See [action.yml](./action.yml)
+
+```yaml
+name: "Assert Branch Naming Convention"
+on: pull_request
+
+jobs:
+  branch-naming-rules:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: scotttball/action-branch-name@master
+        with:
+          regex: '([a-z])+\/([a-z])+' # Regex the branch should match. This example enforces grouping
+          allowed_prefixes: "feature,stable,fix" # All branches should start with the given prefix
+          ignore: master,develop # Ignore exactly matching branch names from convention
+          min_length: 5 # Min length of the branch name
+          max_length: 20 # Max length of the branch name
+```
+
+## License
+
+The scripts and documentation in this project are released under the [MIT License](./LICENSE)
---
+++ index.js
@@ -0,0 +1,85 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { commentPr } = require('./utils/utils.js');
+
+const validEvent = ['push', 'pull_request'];
+
+function getBranchName(eventName, payload) {
+    let branchName;
+    switch (eventName) {
+        case 'push':
+            branchName = payload.ref.replace('refs/heads/', '');
+            break;
+        case 'pull_request':
+            branchName = payload.pull_request.head.ref;
+            break;
+        default:
+            throw new Error(`Invalid event name: ${eventName}`);
+    }
+    return branchName;
+}
+
+async function run() {
+    try {
+        const commentMessage = `Please remake branch name following conventions (ex. JIRA-XXX or JIRA-XXX-branch-name)`,
+            commentMessageWithUserName = `Hi @{USER}! ${commentMessage}`;
+
+        const eventName = github.context.eventName;
+        core.info(`Event name: ${eventName}`);
+        if (validEvent.indexOf(eventName) < 0) {
+            core.setFailed(`Invalid event: ${eventName}`);
+            return;
+        }
+
+        const branch = getBranchName(eventName, github.context.payload);
+        core.info(`Branch name: ${branch}`);
+        // Check if branch is to be ignored
+        const ignore = core.getInput('ignore');
+        if (ignore.length > 0 && ignore.split(',').some((el) => branch === el)) {
+            core.info(`Skipping checks since ${branch} is in the ignored list - ${ignore}`);
+            return
+        }
+
+        // Check if branch pass regex
+        const regex = RegExp(core.getInput('regex'));
+        core.info(`Regex: ${regex}`);
+        if (!regex.test(branch)) {
+            // core.setFailed(`Branch ${branch} failed to pass match regex - ${regex}`);
+            try {
+                await commentPr(commentMessageWithUserName)
+            } catch (e) {
+                core.error(`Error commenting on PR. Status: ${e.status}`)
+            }
+
+            core.setFailed(commentMessage)
+            return
+        }
+
+
+        // Check if branch starts with a prefix
+        const prefixes = core.getInput('allowed_prefixes');
+        core.info(`Allowed Prefixes: ${prefixes}`);
+        if (prefixes.length > 0 && !prefixes.split(',').some((el) => branch.startsWith(el))) {
+            core.setFailed(`Branch ${branch} failed did not match any of the prefixes - ${prefixes}`);
+            return
+        }
+
+        // Check min length
+        const minLen = parseInt(core.getInput('min_length'));
+        if (branch.length < minLen) {
+            core.setFailed(`Branch ${branch} is smaller than min length specified - ${minLen}`);
+            return
+        }
+
+        // Check max length
+        const maxLen = parseInt(core.getInput('max_length'));
+        if (maxLen > 0 && branch.length > maxLen) {
+            core.setFailed(`Branch ${branch} is greater than max length specified - ${maxLen}`);
+            return
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
